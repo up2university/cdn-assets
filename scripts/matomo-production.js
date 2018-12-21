6528b3f323df170ff6d9ac3385b09cfb93c0cd74
Dimitris Pantazatos <dim.pantazatos@gmail.com>
2018-12-21 11:56:52 +0200

    Create matomo-production.js
    Tracking code for the production instances

---
+++ scripts/matomo-production.js
@@ -0,0 +1,13 @@
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDomains", ["*.learn.up2university.eu","*.cernbox.up2university.eu","*.eduoer.up2university.eu","*.lrs.up2university.eu","*.oc.up2university.eu","*.repo.up2university.eu","*.selcont.up2university.eu","*.swan.up2university.eu","*.vc.up2university.eu"]]);
+  _paq.push(["enableCrossDomainLinking"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//matomo.test.up2university.eu/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '3']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
