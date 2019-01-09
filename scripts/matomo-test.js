860446c0c5c43b27eeabd4264240905645bf11ca
Dimitris Pantazatos <dim.pantazatos@gmail.com>
2019-01-09 18:59:15 +0200

    Update matomo-test.js

--- scripts/matomo-test.js
+++ scripts/matomo-test.js
@@ -1,6 +1,6 @@
 var _paq = _paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-_paq.push(["setDomains", ["*.learn.test.up2university.eu","*.cernbox.test.up2university.eu","*.eduoer.test.up2university.eu","*.lrs.test.up2university.eu","*.oc.test.up2university.eu","*.repo.test.up2university.eu","*.selcont.test.up2university.eu","*.swan.test.up2university.eu","*.vc.test.up2university.eu"]]);
+_paq.push(["setDomains", ["*.learn.test.up2university.eu","*.cernbox.test.up2university.eu","*.eduoer.test.up2university.eu","*.lrs.test.up2university.eu","*.oc.test.up2university.eu","*.repo.test.up2university.eu","*.selcont.test.up2university.eu","*.swan.test.up2university.eu","*.vc.test.up2university.eu","*.cs.test.up2university.eu"]]);
 _paq.push(["enableCrossDomainLinking"]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
