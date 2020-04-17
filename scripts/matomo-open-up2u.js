83d28c5831137b1871748f020572937bec5032d9
Michał Zimniewicz <michzimny@users.noreply.github.com>
2020-04-17 23:25:07 +0200

    Update matomo for openUp2U: track landing page too

--- scripts/matomo-open-up2u.js
+++ scripts/matomo-open-up2u.js
@@ -1,7 +1,7 @@
  var _paq = window._paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-  _paq.push(["setDomains", ["*.sso.open.up2university.eu","*.cernbox.open.up2university.eu","*.learn.open.up2university.eu","*.meet01.open.up2university.eu","*.meet02.open.up2university.eu","*.meet03.open.up2university.eu","*.meet04.open.up2university.eu","*.meet05.open.up2university.eu","*.meet06.open.up2university.eu","*.swan.open.up2university.eu"]]);
+  _paq.push(["setDomains", ["up2university.eu","*.sso.open.up2university.eu","*.cernbox.open.up2university.eu","*.learn.open.up2university.eu","*.meet01.open.up2university.eu","*.meet02.open.up2university.eu","*.meet03.open.up2university.eu","*.meet04.open.up2university.eu","*.meet05.open.up2university.eu","*.meet06.open.up2university.eu","*.swan.open.up2university.eu"]]);
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
