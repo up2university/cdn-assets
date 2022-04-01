2cc57332f75689c891da0fd105fb05c2372f06ec
Michal Zimniewicz <mzimniew@man.poznan.pl>
2022-04-01 23:12:48 +0200

    remove matomo tracking codes

--- scripts/matomo-production.js
+++ scripts/matomo-production.js
@@ -1,13 +1,1 @@
-  var _paq = _paq || [];
-  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-  _paq.push(["setDomains", ["*.learn.up2university.eu","*.cernbox.up2university.eu","*.eduoer.up2university.eu","*.lrs.up2university.eu","*.oc.up2university.eu","*.repo.up2university.eu","*.selcont.up2university.eu","*.swan.up2university.eu","*.vc.up2university.eu","*.cs.up2university.eu","*.poddium.eu","*.sso.up2university.eu","*.openedx-up2u.teltek.es","*.studio-up2u.teltek.es","*.meetings.up2university.eu","*.commonspaces.eu"]]);
-  _paq.push(["enableCrossDomainLinking"]);
-  _paq.push(['trackPageView']);
-  _paq.push(['enableLinkTracking']);
-  (function() {
-    var u="//matomo.test.up2university.eu/";
-    _paq.push(['setTrackerUrl', u+'piwik.php']);
-    _paq.push(['setSiteId', '3']);
-    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
-  })();
+
